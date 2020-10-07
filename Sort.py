83a94790701ef687bd7df0301f1e27152023b79e
hackershub11 <72488167+hackershub11@users.noreply.github.com>
2020-10-07 12:00:41 +0530

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -1,3 +1,11 @@
-# Python program for implementation of Bubble Sort 
-  
-def bubbleSort(arr): 
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup 
+quotes="http://intra.iitj.ac.in:8080/Aryabhatta_New/attendanceReport.do"
+uClient=uReq(quotes)
+page_html=uClient.read()
+uClient.close()
+page_soup=soup(page_html, "html.parser")
+quotes=page_soup.findAll("div",{"class":"tagline"})
+print(quotes[0].text.strip())
+
+
