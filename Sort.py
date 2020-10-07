45b073ac8359d5f961096b38090c72e0d2ebf146
Puru <54506517+Puru-Raj@users.noreply.github.com>
2020-10-07 12:04:47 +0530

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as soup
 quotes="http://intra.iitj.ac.in:8080/Aryabhatta_New/attendanceReport.do"
 uClient=uReq(quotes)
 page_html=uClient.read()
-uClient.close()
+
 page_soup=soup(page_html, "html.parser")
 quotes=page_soup.findAll("div",{"class":"tagline"})
 print(quotes[0].text.strip())
